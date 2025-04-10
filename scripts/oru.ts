b9bd5dbef66e611212b851599dc449257db73ce8
Ahmed Kamal <ahmed@moon-i.com.au>
2025-04-10 14:26:48 +1000

    parse oru file

--- scripts/oru.ts
+++ scripts/oru.ts
@@ -1,27 +1,180 @@
+// @ts-expect-error: No types - will add later
 import hl7 from 'tiny-hl7';
-import { differenceInYears } from 'date-fns';
+// @ts-expect-error: No types - will add later
 import HL7Date from 'hl7-date';
+import { differenceInYears } from 'date-fns';
 import * as fs from 'fs';
+import { PrismaClient } from '@prisma/client';
 
-const message = fs
-  .readFileSync('prisma/data/MP826520.oru.txt')
-  .toString()
-  .trim();
+// TODO: add tests here
 
-const parsed = hl7.parse(message);
-// console.log(JSON.stringify(parsed));
+const parsedMessages = hl7.parse(
+  fs.readFileSync('prisma/data/MP826520.oru.txt').toString(),
+) as Record<string, string>[];
 
-const firstName = parsed.find((m) => m['PID.5.1'])['PID.5.2'];
-const lastName = parsed.find((m) => m['PID.5.1'])['PID.5.1'];
-const age = differenceInYears(
-  Date.now(),
-  HL7Date.parse(parsed.find((m) => m['PID.7'])['PID.7']),
-);
-const gender = parsed.find((m) => m['PID.8'])['PID.8'];
+const personalDetails = {
+  firstName: parsedMessages.find((m) => m['PID.5.1'])?.['PID.5.1'],
+  lastName: parsedMessages.find((m) => m['PID.5.1'])?.['PID.5.1'],
+  age: differenceInYears(
+    Date.now(),
+    HL7Date.parse(parsedMessages.find((m) => m['PID.7'])?.['PID.7']),
+  ),
+  gender: ({ M: 'Male', F: 'Female' } as const)[
+    parsedMessages.find((m) => m['PID.8'])?.['PID.8'] as 'M' | 'F'
+  ],
+};
 
-console.log({ M: 'Male', F: 'Female' }[gender]);
+const supportedValueTypes = {
+  NM: 'Numeric',
+  SN: 'Structured Numeric',
+};
 
-// OBX.3.2 code
-// obx.5 value
-// obx.2 type
-// obx.6 unit
+type ResultMessage = {
+  oruSonicCode: string;
+  oruSonicUnit: string;
+  result:
+    | {
+        type: 'NM';
+        value: number;
+      }
+    | {
+        type: 'SN';
+        operater: '<' | '>';
+        value: number;
+      };
+};
+
+const results = parsedMessages
+  .filter(
+    (m) =>
+      m['OBX.3.2'] && Object.keys(supportedValueTypes).includes(m['OBX.2']),
+  )
+  .map(
+    (m): ResultMessage => ({
+      oruSonicCode: m['OBX.3.2'],
+      oruSonicUnit: m['OBX.6.1'],
+      result:
+        m['OBX.2'] === 'NM'
+          ? {
+              type: 'NM',
+              value: parseFloat(m['OBX.5']),
+            }
+          : {
+              type: 'SN',
+              operater: m['OBX.5.1'] as '<' | '>',
+              value: parseFloat(m['OBX.5.2']),
+            },
+    }),
+  );
+
+const prisma = new PrismaClient();
+(async () => {
+  // This is a short list, so should be safe to load it all into memory
+  const metrics = await prisma.metric.findMany({
+    include: {
+      condition: true,
+      diagnostic: {
+        include: { diagnosticGroups: { include: { group: true } } },
+      },
+    },
+  });
+
+  const tmp = results
+    .reduce<
+      { result: (typeof results)[number]; metric: (typeof metrics)[number] }[]
+    >((acc, result) => {
+      const metric = metrics.find((metric) => {
+        const codeMatches = metric.oruSonicCodes.includes(result.oruSonicCode);
+        const unitMatches =
+          metric.oruSonicUnits.includes(result.oruSonicUnit) ||
+          (metric.oruSonicUnits.length === 0 &&
+            result.oruSonicUnit.trim() === '');
+
+        const minAgeMatches =
+          metric.minAge === null || personalDetails.age >= metric.minAge;
+
+        const maxAgeMatches =
+          metric.maxAge === null || personalDetails.age <= metric.maxAge;
+
+        const genderMatches =
+          metric.gender === 'Any' || metric.gender === personalDetails.gender;
+
+        return (
+          codeMatches &&
+          unitMatches &&
+          minAgeMatches &&
+          maxAgeMatches &&
+          genderMatches
+        );
+      });
+      if (!metric) return acc;
+      return [...acc, { result, metric }];
+    }, [])
+    .map(({ result, metric }) => {
+      const atRisk = (
+        lowerLimit: number | null,
+        higherLimit: number | null,
+      ): 'Yes' | 'No' | 'Possible' => {
+        if (result.result.type === 'NM') {
+          return (lowerLimit !== null && result.result.value < lowerLimit) ||
+            (higherLimit !== null && result.result.value > higherLimit)
+            ? 'Yes'
+            : 'No';
+        } else if (result.result.type === 'SN') {
+          if (result.result.operater === '<') {
+            // using <= below because the sign above is <
+            if (lowerLimit !== null && result.result.value <= lowerLimit) {
+              return 'Yes';
+            } else if (
+              higherLimit !== null &&
+              lowerLimit === null &&
+              result.result.value <= higherLimit
+            ) {
+              return 'No';
+            } else if (lowerLimit === null && higherLimit === null) {
+              return 'No';
+            } else return 'Possible';
+          } else if (result.result.operater === '>') {
+            // using >= below because the sign above is >
+            if (higherLimit !== null && result.result.value >= higherLimit) {
+              return 'Yes';
+            } else if (
+              lowerLimit !== null &&
+              higherLimit === null &&
+              result.result.value >= lowerLimit
+            ) {
+              return 'No';
+            } else if (higherLimit === null && lowerLimit === null) {
+              return 'No';
+            } else return 'Possible';
+          }
+        }
+
+        throw new Error('atRisk should handle all cases');
+      };
+
+      return {
+        metricName: metric.name,
+        metricUnits: metric.units,
+        metricStandardLower: metric.standardLower,
+        metricStandardHigher: metric.standardHigher,
+        metricEverlabLower: metric.everlabLower,
+        metricEverlabHigher: metric.everlabHigher,
+
+        conditionName: metric.condition?.name,
+        diagnosticName: metric.diagnostic?.name,
+        groupNames:
+          metric.diagnostic?.diagnosticGroups.map(
+            (diagnosticGroup) => diagnosticGroup.group.name,
+          ) ?? [],
+
+        resultValue:
+          result.result.type === 'NM'
+            ? `${result.result.value}`
+            : `${result.result.operater} ${result.result.value}`,
+        standardAtRisk: atRisk(metric.standardLower, metric.standardHigher),
+        everlabAtRisk: atRisk(metric.everlabLower, metric.everlabHigher),
+      };
+    });
+  console.log(JSON.stringify(tmp, null, 2));
+})();
